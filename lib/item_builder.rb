f00616722659dae10efb0ef9f9e2c9096177cee6
Bradley Sheehan <bradpsheehan@gmail.com>
2013-02-13 19:23:55 -0700

    Added all fields for Item class's initialize method

--- lib/item_builder.rb
+++ lib/item_builder.rb
@@ -7,14 +7,14 @@ class ItemBuilder
     contents = CSV.open(file, headers: true, header_converters: :symbol)
 
     @data = contents.collect do |item|
-      item_hash = {}
-      item_hash[:id] = item[:id]
-      item_hash[:name] = item[:name]
+      item_hash               = {}
+      item_hash[:id]          = item[:id]
+      item_hash[:name]        = item[:name]
       item_hash[:description] = item[:description]
-      item_hash[:unit_price] = item[:unit_price]
+      item_hash[:unit_price]  = item[:unit_price]
       item_hash[:merchant_id] = item[:merchant_id]
-      item_hash[:created_at] = item[:created_at]
-      item_hash[:updated_at] = item[:updated_at]
+      item_hash[:created_at]  = item[:created_at]
+      item_hash[:updated_at]  = item[:updated_at]
 
       Item.new(item_hash)
     end
