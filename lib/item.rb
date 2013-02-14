3bfc303b492af6ee9c48bf7409f98cbda6c90784
Bradley Sheehan <bradpsheehan@gmail.com>
2013-02-13 18:50:59 -0700

    loaded and tested items csv file

---
+++ lib/item.rb
@@ -0,0 +1,10 @@
+class Item
+
+attr_reader :id, :name
+
+  def initialize(data)
+    @id   = data[:id]
+    @name = data[:name]
+  end
+
+end
---
+++ lib/item_builder.rb
@@ -0,0 +1,23 @@
+require 'csv'
+require './lib/item'
+
+class ItemBuilder
+
+  def self.parse_csv(file)
+    contents = CSV.open(file, headers: true, header_converters: :symbol)
+
+    @data = contents.collect do |item|
+      item_hash = {}
+      item_hash[:id] = item[:id]
+      item_hash[:name] = item[:name]
+      item_hash[:description] = item[:description]
+      item_hash[:unit_price] = item[:unit_price]
+      item_hash[:merchant_id] = item[:merchant_id]
+      item_hash[:created_at] = item[:created_at]
+      item_hash[:updated_at] = item[:updated_at]
+
+      Item.new(item_hash)
+    end
+  end
+
+end
