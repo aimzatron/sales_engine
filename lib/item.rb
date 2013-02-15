6945aa33799eb2b01866913679356eb5ac587a06
Kareem Grant <kareem@getuserwise.com>
2013-02-14 17:24:06 -0700

    completed and tested item searching feature

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,11 @@
 class Item
 
-  attr_reader :id, :name, :descrption, :unit_price, :merchant_id, :created_at, :updated_at
+  attr_reader :id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at
 
   def initialize(data)
     @id          = data[:id]
     @name        = data[:name]
-    @descrption  = data[:descrption]
+    @description  = data[:description]
     @unit_price  = data[:unit_price]
     @merchant_id = data[:merchant_id]
     @created_at  = data[:created_at]
@@ -18,6 +18,50 @@ class Item
 
   def self.all
     @data
+  end
+
+  def self.random
+    @data.sample
+  end
+
+  def self.find_by_name(name)
+    @data.find{|item| item.name.downcase == name.downcase}
+  end
+
+  def self.find_all_by_name(name)
+    @data.select{|item| item.name.downcase == name.downcase}
+  end
+
+  def self.find_by_id(id)
+    @data.find{|item| item.id.downcase == id.downcase}
+  end
+
+  def self.find_all_by_id(id)
+    @data.select{|item| item.id.downcase == id.downcase}
+  end
+
+  def self.find_by_desc(desc)
+    @data.find{|item| item.description.downcase == desc.downcase}
+  end
+
+  def self.find_all_by_desc(desc)
+    @data.select{|item| item.description.downcase == desc.downcase}
+  end
+
+  def self.find_by_unit_price(unit_price)
+    @data.find{|item| item.unit_price.downcase == unit_price.downcase}
+  end
+
+  def self.find_all_by_unit_price(unit_price)
+    @data.select{|item| item.unit_price.downcase == unit_price.downcase}
+  end
+
+  def self.find_by_merchant_id(merchant_id)
+    @data.find{|item| item.merchant_id.downcase == merchant_id.downcase}
+  end
+
+  def self.find_all_by_merchant_id(merchant_id)
+    @data.select{|item| item.merchant_id.downcase == merchant_id.downcase}
   end
 
 end
